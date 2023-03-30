4d124368fe0c2c22d3a8ed625c00e8b7b6b763ff
dharanicb <dharanicb86391@gmail.com>
2023-03-30 14:47:22 +0000

    solution recent

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const Notification = props => {
   //  Write your code here.
-  const {className , textContent, url} = props;
+  const containerClassName = {`box-content ${className}`}
+
   return (
-  <div className = {`${className}`}>
-      <img className = {`${className}`} src = {`${url}`}/>
-      <p className = {`${className}`}>{textContent}</p>
-  </div>
-  );
-}
+    <div className={containerClassName}>
+      <img className="icon" src={iconUrl} />
+      <p className="text-content">{textContent}</p>
+    </div>
+  )
+};
 
 const element = (
   //  Write your code here.   
-<no
+  <div className = "flex-content">
+  <h1 className = "heading">Notification</h1>
+   <Notification className = "box" iconUrl = "https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" textContent = "Information Massage"></Notification>
+   <Notification className = "box1" iconUrl = "https://assets.ccbp.in/frontend/react-js/success-icon-img.png" textContent = "Information Massage"></Notification>
+   <Notification className = "box2" iconUrl = "https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" textContent = "Information Massage"></Notification>
+   <Notification className = "box3" iconUrl = "https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" textContent = "Information Massage"></Notification>
+</div>
 
 )
 
