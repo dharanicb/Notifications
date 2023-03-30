315398aca767c3b9be9249ddd63e5296744cf20b
dharanicb <dharanicb86391@gmail.com>
2023-03-30 04:39:15 +0000

    solution

---
+++ index.js
@@ -0,0 +1,18 @@
+const Notification = props => {
+  //  Write your code here.
+  const {className , textContent, url} = props;
+  return (
+  <div className = {`${className}`}>
+      <img className = {`${className}`} src = {`${url}`}/>
+      <p className = {`${className}`}>{textContent}</p>
+  </div>
+  );
+}
+
+const element = (
+  //  Write your code here.   
+<no
+
+)
+
+ReactDOM.render(element, document.getElementById('root'))
