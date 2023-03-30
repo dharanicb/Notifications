08439eca6b29015504fa78cdd79a05048ce7e766
dharanicb <dharanicb86391@gmail.com>
2023-03-30 16:10:34 +0000

    solution rec

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const element = (
   //  Write your code here.   
   <div className = "flex-content">
   <h1 className = "heading">Notification</h1>
-   <Notification className = "box" iconUrl = "https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" textContent = "Information Massage"></Notification>
-   <Notification className = "box1" iconUrl = "https://assets.ccbp.in/frontend/react-js/success-icon-img.png" textContent = "Information Massage"></Notification>
-   <Notification className = "box2" iconUrl = "https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" textContent = "Information Massage"></Notification>
-   <Notification className = "box3" iconUrl = "https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" textContent = "Information Massage"></Notification>
+   <Notification className = "box" iconUrl = "https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" textContent = "Information Massage"/>
+   <Notification className = "box1" iconUrl = "https://assets.ccbp.in/frontend/react-js/success-icon-img.png" textContent = "Information Massage"/>
+   <Notification className = "box2" iconUrl = "https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" textContent = "Information Massage"/>
+   <Notification className = "box3" iconUrl = "https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" textContent = "Information Massage"/>
 </div>
 
 )
