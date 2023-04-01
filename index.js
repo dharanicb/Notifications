af1f655c4b3093b8593d6b7c62fce7c5237477e5
dharanicb <dharanicb86391@gmail.com>
2023-04-01 04:01:34 +0000

    solution recent

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const Notification = (props) => {
   //  Write your code here.
-  const {className , iconUrl, textContent} = props;
-  const containerClassName = {`box-content ${className}`}
+  const { className, iconUrl, textContent } = props;
+  const containerClassName = `box-content ${className}`;
 
   return (
     <div className={containerClassName}>
       <img className="icon" src={iconUrl} />
       <p className="text-content">{textContent}</p>
     </div>
-  )
+  );
 };
 
 const element = (
-  //  Write your code here.   
-  <div className = "flex-content">
-  <h1 className = "heading">Notification</h1>
-   <Notification className = "box" iconUrl = "https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" textContent = "Information Massage"/>
-   <Notification className = "box1" iconUrl = "https://assets.ccbp.in/frontend/react-js/success-icon-img.png" textContent = "Information Massage"/>
-   <Notification className = "box2" iconUrl = "https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" textContent = "Information Massage"/>
-   <Notification className = "box3" iconUrl = "https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" textContent = "Information Massage"/>
-</div>
+  //  Write your code here.
+  <div className="flex-content">
+    <h1 className="heading">Notification</h1>
+    <Notification
+      className="box"
+      iconUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+      textContent="Information Massage"
+    />
+    <Notification
+      className="box1"
+      iconUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+      textContent="Information Massage"
+    />
+    <Notification
+      className="box2"
+      iconUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+      textContent="Information Massage"
+    />
+    <Notification
+      className="box3"
+      iconUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+      textContent="Information Massage"
+    />
+  </div>
+);
 
-)
-
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById("root"));
